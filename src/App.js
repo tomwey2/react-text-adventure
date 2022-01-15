1f6473ccd7b1eb960393b4c00747dcbcf2b8cf72
Thomas <tomwey2@gmail.com>
2022-01-15 17:05:16 +0400

    implemented simple text adventure functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import json from "./adventure.json";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      situationId: 0
+    };
+  }
+
+  gotoSituation(id) {
+    this.setState({
+      situationId: id
+    });
+  }
+
+  render() {
+    let situation = json.situations[this.state.situationId];
+    return (
+      <div className="retro">
+        <h3>*** Die Mumie des Schreckens ***</h3>
+        {/* the situations description text */}
+        <Situation
+          situation={situation}
+          onClick={goal => this.gotoSituation(goal)}
+        />
+      </div>
+    );
+  }
+}
+
+class Situation extends React.Component {
+  getAction(situationId) {
+    for (let i in json.situations) {
+      if (json.situations[i].id === situationId) {
+        return json.situations[i].action;
+      }
+    }
+  }
+
+  render() {
+    let actions = [];
+    for (const [index, value] of this.props.situation.goals.entries()) {
+      let action = this.getAction(value);
+      console.log(action);
+      actions.push(
+        <li key={index}>
+          <Action onClick={() => this.props.onClick(value)} action={action} />
+        </li>
+      );
+    }
+
+    return (
+      <div>
+        <div
+          dangerouslySetInnerHTML={{
+            __html: this.props.situation.text
+          }}
+        />
+        <ul>{actions}</ul>
+      </div>
+    );
+  }
+}
+
+function Action(props) {
+  return <div onClick={goal => props.onClick(goal)}>{props.action}</div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+class HelloMessage extends React.Component {
+  render() {
+    return <div>Hello {this.props.name}</div>;
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  //  <HelloMessage name="John Doe" />,
+  <App />,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
